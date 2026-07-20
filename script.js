cf9818f001d4660eea1b034f4424db4f30cae62e
yun2668 <qing77891@gmail.com>
2026-07-20 16:27:18 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+body {
+    text-align:center;
+    font-family:Arial;
+    background:#eeeeee;
+}
+
+input {
+    width:220px;
+    height:40px;
+    font-size:25px;
+    margin-bottom:10px;
+}
+
+button {
+    width:50px;
+    height:50px;
+    margin:5px;
+    font-size:20px;
+}
